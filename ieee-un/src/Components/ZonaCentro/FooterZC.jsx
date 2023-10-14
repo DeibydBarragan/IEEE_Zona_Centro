eb7938248711a6a04466a57f3239f7e9d8f52af8
William Pinilla <wpinilla@unal.edu.co>
2023-10-13 21:57:05 -0500

    Agregado footer

--- ieee-un/src/Components/ZonaCentro/FooterZC.jsx
+++ ieee-un/src/Components/ZonaCentro/FooterZC.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import paginaIcon from "../../Assets/zonaCentro/Icono_IEEE.png"
+import "../../Styles/FooterZC.sass"
 export default  function FooterZC() {
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className="footer">
+      <div className="footer-titulo">
+        <h2>IEEE UN</h2>
+        <img src={paginaIcon} alt="" />
+      </div>
+      <div className="footer-Iconos_redes">
+        <a href="https://www.facebook.com/ieeeunbog/?locale=es_LA"><span className="fb_Icon"></span></a>
+        <a href="https://www.instagram.com/ramaieeeun/?hl=es"><span className="ig_Icon"></span></a>
+        <a href="https://www.linkedin.com/company/ieee-un/?originalSubdomain=co"><span className="in_Icon"></span></a>
+      </div>
     </div>
   );
 }
