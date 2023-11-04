71662cd1c501433d0b9c5be801eb9411750c6319
William Pinilla <wpinilla@unal.edu.co>
2023-11-03 22:05:40 -0500

    Agregada seccion opiniones e invitados

--- ieee-un/src/Components/ZonaCentro/Sections/Guests.jsx
+++ ieee-un/src/Components/ZonaCentro/Sections/Guests.jsx
@@ -1,8 +1,39 @@
 import React from "react";
+import "../../../Styles/invitadosZC.sass"
+
+import img1 from "./../../../Assets/zonaCentro/image_1.png"
+import img2 from "./../../../Assets/zonaCentro/image_2.png"
+import img3 from "./../../../Assets/zonaCentro/image_3.png"
+import img4 from "./../../../Assets/zonaCentro/image_4.png"
+
 export default  function Guests() {
   return (
-    <div className="guests">
-      <h1>Guests</h1>
+    <div id="guests">
+      <h2>Invitados</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero explicabo odio optio excepturi, quos neque iusto, ipsum et sed eaque rerum ducimus deleniti laudantium reprehenderit inventore modi eius in quisquam?
+      Quos, ullam est. Asperiores voluptas nemo a ipsa suscipit minima pariatur facere quasi, quaerat enim officiis aliquam accusantium excepturi dignissimos tempora quos facilis laboriosam. Laudantium quia impedit quidem nam quam.</p>
+      <div className="guestsFotos">
+        <article>
+          <img src={img1} alt="" />
+          <h3>Lorem Ipsum</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </article>
+        <article>
+          <img src={img2} alt="" />
+          <h3>Lorem Ipsum</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </article>
+        <article>
+          <img src={img3} alt="" />
+          <h3>Lorem Ipsum</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </article>
+        <article>
+          <img src={img4} alt="" />
+          <h3>Lorem Ipsum</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </article>
+      </div>
     </div>
   );
 }
--- ieee-un/src/Components/ZonaCentro/Sections/Opinions.jsx
+++ ieee-un/src/Components/ZonaCentro/Sections/Opinions.jsx
@@ -1,8 +1,63 @@
 import React from "react";
+import "../../../Styles/OpinionsZC.sass"
+
+import Carousel from 'react-bootstrap/Carousel';
+import JohnChina from "./../../../Assets/zonaCentro/john-china.jpg"
+import quote from "./../../../Assets/zonaCentro/icon_quote.png"
+
 export default  function Opinions() {
   return (
     <div id="opinions">
-      <h1>Opinions</h1>
+      <div className="carrusel">
+        <Carousel variant="dark">
+          <Carousel.Item>
+            <div className="carrItem1">
+              <img src={quote} alt="" />
+              <p>
+                “ESTE EVENTO ME CAMBIÓ LA VIDA. CONOCÍ EL CAPÍTULO DE ROBÓTICA Y LOS CHICOS FUERON MUY AMABLES. YA SÉ EN QUÉ QUIERO TRABAJAR CUANDO ME GRADÚE”
+              </p>
+              <div className="carrPersona">
+                <img src={JohnChina} alt="" />
+                <div className="carDatos">
+                  <h3 className="carDatos-Titulo">John Xina</h3>
+                  <h3>Estudiante</h3>
+                </div>
+              </div>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item>
+          <div className="carrItem2">
+              <img src={quote} alt="" />
+              <p>
+                “ESTE EVENTO ME CAMBIÓ LA VIDA. CONOCÍ EL CAPÍTULO DE ROBÓTICA Y LOS CHICOS FUERON MUY AMABLES. YA SÉ EN QUÉ QUIERO TRABAJAR CUANDO ME GRADÚE”
+              </p>
+              <div className="carrPersona">
+                <img src={JohnChina} alt="" />
+                <div className="carDatos">
+                  <h3 className="carDatos-Titulo">John Xina</h3>
+                  <h3>Estudiante</h3>
+                </div>
+              </div>
+            </div>
+          </Carousel.Item>
+          <Carousel.Item>
+          <div className="carrItem1">
+              <img src={quote} alt="" />
+              <p>
+                “ESTE EVENTO ME CAMBIÓ LA VIDA. CONOCÍ EL CAPÍTULO DE ROBÓTICA Y LOS CHICOS FUERON MUY AMABLES. YA SÉ EN QUÉ QUIERO TRABAJAR CUANDO ME GRADÚE”
+              </p>
+              <div className="carrPersona">
+                <img src={JohnChina} alt="" />
+                <div className="carDatos">
+                  <h3 className="carDatos-Titulo">John Xina</h3>
+                  <h3>Estudiante</h3>
+                </div>
+              </div>
+            </div>
+          </Carousel.Item>
+        </Carousel>
+      </div>
+      <h2>Algunos comentarios acerca del evento</h2>
     </div>
   );
 }
