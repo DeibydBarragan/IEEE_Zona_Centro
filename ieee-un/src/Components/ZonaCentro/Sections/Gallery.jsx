40e288d163a0e1ecf2cba2e636f7f87c9112e12f
DeibydBarragan <116578796+DeibydBarragan@users.noreply.github.com>
2023-11-18 00:40:34 -0500

    Refactorización de sección de galería
    ahora se le puede pasar un array de imagenes y se acomodan

--- ieee-un/src/Components/ZonaCentro/Sections/Gallery.jsx
+++ ieee-un/src/Components/ZonaCentro/Sections/Gallery.jsx
@@ -4,47 +4,40 @@ import Row from "react-bootstrap/Row";
 import {MdKeyboardArrowDown, MdKeyboardArrowUp} from "react-icons/md";
 import heroImage from "../../../Assets/zonaCentro/landing/hero.jpeg"
 import {Button, Collapse} from "react-bootstrap";
+import ImagesGroup from "../Components/ImagesGroup";
+
+const IMAGES = [heroImage,
+  heroImage,
+  heroImage,
+  heroImage,
+  heroImage,
+  heroImage,
+  heroImage,
+  heroImage,
+  heroImage,
+  heroImage,
+  heroImage,
+]
+
 export default  function Gallery() {
   const [open, setOpen] = useState(false);
   return (
     <Container fluid as="section" id="gallery" className="gallery">
       <h2>Galería</h2>
       <Row className="gap-2 justify-content-center mt-4">
-        <div className="col col-5">
-          <img src={ heroImage } alt="Evento Zona Centro" />
-        </div>
-        <div className="col col-3">
-          <img src={ heroImage } alt="Evento Zona Centro" />
-          <img src={ heroImage } alt="Evento Zona Centro" />
-        </div>
-        <div className="col col-3">
-          <img src={ heroImage } alt="Evento Zona Centro" />
-          <img src={ heroImage } alt="Evento Zona Centro" />
-        </div>
+        <ImagesGroup images={IMAGES.slice(0, 5)} alt="Evento Zona Centro" />
       </Row>
-      <Row className="justify-content-center mt-4">
+      <Row className="justify-content-center">
         <Collapse in={open}>
           <div>
-            <Row className="gap-2 justify-content-center mb-4 flex-row-reverse">
-              <div className="col col-5">
-                <img src={ heroImage } alt="Evento Zona Centro" />
-              </div>
-              <div className="col col-3">
-                <img src={ heroImage } alt="Evento Zona Centro" />
-                <img src={ heroImage } alt="Evento Zona Centro" />
-              </div>
-              <div className="col col-3">
-                <img src={ heroImage } alt="Evento Zona Centro" />
-                <img src={ heroImage } alt="Evento Zona Centro" />
-              </div>
-            </Row>
+            <ImagesGroup images={IMAGES.slice(5)} alt="Evento Zona Centro" reverse />
           </div>
         </Collapse>
         <Button
           onClick={() => setOpen(!open)}
           aria-controls="Ver más imágenes"
           aria-expanded={open}
-          className="text-center"
+          className="text-center mt-2"
         >
           {open ? <MdKeyboardArrowUp size={25}/> : <MdKeyboardArrowDown size={25}/>}
         </Button>
